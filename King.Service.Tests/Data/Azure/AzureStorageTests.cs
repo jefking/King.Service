6c649d970ba5042b91258389518aff63d684fb88
Jef King <jef.king@trader.ca>
2014-05-30 09:42:10 -0700

    Test Cases

---
+++ King.Service.Tests/Data/Azure/AzureStorageTests.cs
@@ -0,0 +1,23 @@
+﻿namespace King.Service.Tests.Data.Azure
+{
+    using System;
+    using King.Service.Data.Azure;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class AzureStorageTests
+    {
+        [TestMethod]
+        public void Constructor()
+        {
+            new AzureStorage("TestKey");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConstructorNull()
+        {
+            new AzureStorage(null);
+        }
+    }
+}
