564077f633b5724203ef1c34c11a5c9e46a79d8a
Jef King <jef.king@trader.ca>
2014-06-02 09:25:40 -0700

    Test cases

---
+++ King.Service.Tests/Data/ScheduledTaskCoreTests.cs
@@ -0,0 +1,23 @@
+﻿namespace King.Service.Tests.Data
+{
+    using System;
+    using King.Service.Data;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ScheduledTaskCoreTests
+    {
+        [TestMethod]
+        public void Constructor()
+        {
+            new ScheduledTaskCore(new TimeSpan(9000), "TestKey");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConstructorTimeSpanZero()
+        {
+            new ScheduledTaskCore(new TimeSpan(0), "TestKey");
+        }
+    }
+}
