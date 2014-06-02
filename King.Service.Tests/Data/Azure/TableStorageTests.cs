ea314a74ae455084741bb534253a7a1f81b458d3
Jef King <jef.king@trader.ca>
2014-06-02 09:17:57 -0700

    Test Cases UPdated

---
+++ King.Service.Tests/Data/Azure/TableStorageTests.cs
@@ -0,0 +1,30 @@
+﻿namespace King.Service.Tests.Data.Azure
+{
+    using System;
+    using King.Service.Data.Azure;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TableStorageTests
+    {
+        [TestMethod]
+        public void Constructor()
+        {
+            new TableStorage("TestTable", "TestKey");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConstructorTableNull()
+        {
+            new TableStorage(null, "TestKey");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConstructorKeyNull()
+        {
+            new TableStorage("TestTable", null);
+        }
+    }
+}
