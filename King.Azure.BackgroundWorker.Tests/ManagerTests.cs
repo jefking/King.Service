20066ba88cb4360f441c04542bba5128dbc62eb7
Jef King <jef.king@trader.ca>
2014-06-24 15:32:05 -0700

    remove usings

--- King.Azure.BackgroundWorker.Tests/ManagerTests.cs
+++ King.Azure.BackgroundWorker.Tests/ManagerTests.cs
@@ -1,11 +1,6 @@
 ﻿namespace King.Azure.BackgroundWorker.Tests
 {
     using Microsoft.VisualStudio.TestTools.UnitTesting;
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     [TestClass]
     public class ManagerTests
