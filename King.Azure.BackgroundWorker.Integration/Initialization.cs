c03e6e4ad42a974c0f4425b6b1a9cd7b2914e562
Jef King <jamieking80@gmail.com>
2014-06-24 19:55:29 -0700

    running integration tests

--- King.Azure.BackgroundWorker.Integration/Initialization.cs
+++ King.Azure.BackgroundWorker.Integration/Initialization.cs
@@ -1,12 +1,37 @@
 ﻿namespace King.Azure.BackgroundWorker.Integration
 {
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+    using System.Configuration;
+    using System.Diagnostics;
 
+    [TestClass]
     public class Initialization
     {
+        [AssemblyInitialize]
+        public static void Init(TestContext context)
+        {
+            var emulator = ConfigurationManager.AppSettings["AzureEmulator"];
+
+            using (var process = new Process())
+            {
+                process.StartInfo = CreateProcessStartInfo(emulator, "/devstore:start");
+                process.Start();
+
+                process.WaitForExit();
+            }
+        }
+
+        private static ProcessStartInfo CreateProcessStartInfo(string fileName, string arguments)
+        {
+            return new ProcessStartInfo(fileName, arguments)
+            {
+                UseShellExecute = false,
+                ErrorDialog = false,
+                CreateNoWindow = true,
+                RedirectStandardError = true,
+                RedirectStandardOutput = true,
+            };
+        }
     }
 }
