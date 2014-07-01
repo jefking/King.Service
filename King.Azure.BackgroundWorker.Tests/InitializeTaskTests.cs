a5d7ec2d1ecae958b25015a171e5d4a5655217ea
Jef King <jamieking80@gmail.com>
2014-06-30 20:17:01 -0700

    Initialize Task

---
+++ King.Azure.BackgroundWorker.Tests/InitializeTaskTests.cs
@@ -0,0 +1,27 @@
+﻿namespace King.Azure.BackgroundWorker.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class InitializeTaskTests
+    {
+        public class Helper : InitializeTask
+        {
+            public override void Run()
+            {
+            }
+        }
+
+        [TestMethod]
+        public void Constructor()
+        {
+            new Helper();
+        }
+
+        [TestMethod]
+        public void IsTaskManager()
+        {
+            Assert.IsNotNull(new Helper() as TaskManager);
+        }
+    }
+}
---
+++ King.Azure.BackgroundWorker/InitializeTask.cs
@@ -0,0 +1,20 @@
+﻿namespace King.Azure.BackgroundWorker
+{
+    using System;
+
+    /// <summary>
+    /// Initialize Task
+    /// </summary>
+    public abstract class InitializeTask : TaskManager
+    {
+        #region Constructors
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public InitializeTask()
+            :base(5, TimeSpan.FromDays(29.5d).TotalSeconds)
+        {
+        }
+        #endregion
+    }
+}
