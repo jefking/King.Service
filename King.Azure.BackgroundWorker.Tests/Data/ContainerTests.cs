11bd000822f38a693c87cf702bda91cf4dd196d3
Jef King <jamieking80@gmail.com>
2014-07-02 21:42:48 -0700

    Container

---
+++ King.Azure.BackgroundWorker.Tests/Data/ContainerTests.cs
@@ -0,0 +1,44 @@
+﻿namespace King.Azure.BackgroundWorker.Tests.Data
+{
+    using King.Azure.BackgroundWorker.Data;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+
+    [TestClass]
+    public class ContainerTests
+    {
+        [TestMethod]
+        public void Constructor()
+        {
+            new Container("test", "UseDevelopmentStorage=true");
+        }
+
+        [TestMethod]
+        public void IsIAzureStorage()
+        {
+            Assert.IsNotNull(new Container("test", "UseDevelopmentStorage=true") as IAzureStorage);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConstructorTableNull()
+        {
+            new Container(null, "UseDevelopmentStorage=true");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConstructorKeyNull()
+        {
+            new Container("test", null);
+        }
+
+        [TestMethod]
+        public void Name()
+        {
+            var name = Guid.NewGuid().ToString();
+            var t = new Container(name, "UseDevelopmentStorage=true");
+            Assert.AreEqual(name, t.Name);
+        }
+    }
+}
---
+++ King.Azure.BackgroundWorker/Data/Container.cs
@@ -0,0 +1,67 @@
+﻿namespace King.Azure.BackgroundWorker.Data
+{
+    using Microsoft.WindowsAzure.Storage.Blob;
+    using Microsoft.WindowsAzure.Storage.Queue;
+    using System;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Queue
+    /// </summary>
+    public class Container : AzureStorage, IAzureStorage
+    {
+        #region Members
+        /// <summary>
+        /// Client
+        /// </summary>
+        private CloudBlobClient client;
+
+        /// <summary>
+        /// Reference
+        /// </summary>
+        private CloudBlobContainer reference;
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Queue
+        /// </summary>
+        /// <param name="name">Name</param>
+        public Container(string name, string connectionStringKey)
+            : base(connectionStringKey)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("name");
+            }
+
+            this.client = this.account.CreateCloudBlobClient();
+            this.reference = this.client.GetContainerReference(name);
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Table Name
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return this.reference.Name;
+            }
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Create If Not Exists
+        /// </summary>
+        /// <returns></returns>
+        public async Task<bool> CreateIfNotExists()
+        {
+            return await this.reference.CreateIfNotExistsAsync();
+        }
+        #endregion
+    }
+}
