28d258a230569cbac5c2a5273076d5fe827a605c
Jef King <jamieking80@gmail.com>
2014-07-07 23:55:36 -0700

    Exponential Backoff Timing

---
+++ King.Azure.BackgroundWorker.Tests/TimingTests.cs
@@ -0,0 +1,68 @@
+﻿namespace King.Azure.BackgroundWorker.Tests
+{
+
+using NUnit.Framework;
+    using System;
+
+    [TestFixture]
+    public class TimingTests
+    {
+        [Test]
+        public void Constructor()
+        {
+            new Timing();
+        }
+
+        [Test]
+        public void Attempt()
+        {
+            var random = new Random();
+            var min = random.Next();
+            var time = new Timing();
+            var ex = time.Exponential(min, 60, 0);
+            Assert.AreEqual(min, ex);
+        }
+
+        [Test]
+        public void AttemptMax()
+        {
+            var random = new Random();
+            var max = random.Next(1, 60);
+            var time = new Timing();
+            var ex = time.Exponential(0, max, random.Next(61));
+            Assert.AreEqual(max, ex);
+        }
+
+        [Test]
+        public void FirstAttempt()
+        {
+            var time = new Timing();
+            var ex = time.Exponential(0, 60, 1);
+            Assert.AreEqual(2, ex);
+        }
+        
+        [Test]
+        public void SecondAttempt()
+        {
+            var time = new Timing();
+            var ex = time.Exponential(0, 60, 2);
+            Assert.AreEqual(4, ex);
+        }
+
+        [Test]
+        public void ThirdAttempt()
+        {
+            var time = new Timing();
+            var ex = time.Exponential(0, 60, 3);
+            Assert.AreEqual(8, ex);
+        }
+
+        [Test]
+        public void FourthAttempt()
+        {
+            var time = new Timing();
+            var ex = time.Exponential(0, 60, 4);
+            Assert.AreEqual(16, ex);
+        }
+    }
+}
---
+++ King.Azure.BackgroundWorker/Timing.cs
@@ -0,0 +1,29 @@
+﻿namespace King.Azure.BackgroundWorker
+{
+    using System;
+
+    /// <summary>
+    /// Timing Maths
+    /// </summary>
+    public class Timing
+    {
+        #region Methods
+        /// <summary>
+        /// Exponential Backoff strategy, within bounds
+        /// </summary>
+        /// <param name="min">lower bound</param>
+        /// <param name="max">upper bound</param>
+        /// <param name="attempts">attempts</param>
+        /// <returns>timing</returns>
+        public double Exponential(int min = 0, int max = 60, int attempts = 0)
+        {
+            if (0 == attempts)
+            {
+                return min;
+            }
+            var current = Math.Pow(2, attempts);
+            return current < max ? current : max;
+        }
+        #endregion
+    }
+}
