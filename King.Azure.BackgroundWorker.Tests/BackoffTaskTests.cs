aff18909eea168925964aea65d641892cb7cd084
Jef King <jamieking80@gmail.com>
2014-07-08 13:42:43 -0700

    tests

---
+++ King.Azure.BackgroundWorker.Tests/BackoffTaskTests.cs
@@ -0,0 +1,23 @@
+﻿namespace King.Azure.BackgroundWorker.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class BackoffTaskTests
+    {
+        #region Helper
+        public class BackoffTest : BackoffTask
+        {
+            public bool Work
+            {
+                get;
+                set;
+            }
+            public override void Run(out bool workWasDone)
+            {
+                workWasDone = this.Work;
+            }
+        }
+        #endregion
+    }
+}
