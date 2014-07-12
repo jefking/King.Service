59c10d5abc0ddc3e24ac66ec5b5de6411aaf56af
Jef King <jamieking80@gmail.com>
2014-07-12 09:54:33 -0700

    dequeue stubbed out.

---
+++ King.Azure.BackgroundWorker.Tests/DequeueTests.cs
@@ -0,0 +1,31 @@
+﻿namespace King.Azure.BackgroundWorker.Tests
+{
+    using King.Azure.BackgroundWorker.Data;
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture]
+    public class DequeueTests
+    {
+        [Test]
+        public void Constructor()
+        {
+            new Dequeue();
+        }
+
+        [Test]
+        public void IsBackoffTask()
+        {
+            Assert.IsNotNull(new Dequeue() as BackoffTask);
+        }
+
+        [Test]
+        [ExpectedException(typeof(NotImplementedException))]
+        public void Run()
+        {
+            var work = false;
+            var d = new Dequeue();
+            d.Run(out work);
+        }
+    }
+}
---
+++ King.Azure.BackgroundWorker/Data/Dequeue.cs
@@ -0,0 +1,12 @@
+﻿namespace King.Azure.BackgroundWorker.Data
+{
+    using System;
+
+    public class Dequeue : BackoffTask
+    {
+        public override void Run(out bool workWasDone)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
