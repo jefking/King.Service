64d8ef9118ccf9d501d12bf278020d902bb9ddcd
Jef King <jamieking80@gmail.com>
2014-07-13 20:54:06 -0700

    testing

---
+++ King.Azure.BackgroundWorker.Tests/Data/DequeueTests.cs
@@ -0,0 +1,35 @@
+﻿namespace King.Azure.BackgroundWorker.Tests.Data
+{
+    using King.Azure.BackgroundWorker.Data;
+    using NSubstitute;
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture]
+    public class DequeueTests
+    {
+        [Test]
+        public void Constructor()
+        {
+            var poller = Substitute.For<IPoller<object>>();
+            var processor = Substitute.For<IProcessor<object>>();
+            new Dequeue<object>(poller, processor);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ConstructorPollerNull()
+        {
+            var processor = Substitute.For<IProcessor<object>>();
+            new Dequeue<object>(null, processor);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ConstructorProcessorNull()
+        {
+            var poller = Substitute.For<IPoller<object>>();
+            new Dequeue<object>(poller, null);
+        }
+    }
+}
