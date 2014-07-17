a7b7e889da55d2695139cd8deb01bcca6d83ae57
Jef King <jamieking80@gmail.com>
2014-07-17 11:46:00 -0700

    nuspec released

--- Worker/Queue/CompanyDequeuer.cs
+++ Worker/Queue/CompanyDequeuer.cs
@@ -8,7 +8,7 @@
     public class CompanyDequeuer : StorageDequeue<CompanyModel>
     {
         public CompanyDequeuer(string connectionString)
-            : base("queue", connectionString, new CompanyProcessor(), 15, 45)
+            : base("queue", connectionString, new CompanyProcessor())
         {
         }
     }
