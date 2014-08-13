17b27de85cc8cb84eb923aaba8bff5916413fb71
Jef King <jef@agilebusinesscloud.com>
2014-08-12 21:18:53 -0700

    Tracing

--- Worker/SelfGoverning.cs
+++ Worker/SelfGoverning.cs
@@ -2,12 +2,15 @@
 {
     using King.Service;
     using System;
+    using System.Diagnostics;
 
     public class SelfGoverning : SelfGoverningTask
     {
         public override void Run(out bool workWasDone)
         {
             workWasDone = (DateTime.Now.Ticks % 2) == 0;
+
+            Trace.TraceInformation("Work was done: {0}", workWasDone);
         }
     }
 }
