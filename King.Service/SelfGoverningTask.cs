fe0a62becf632f8af914c00f03e04b64abb459dd
Jef King <jef@agilebusinesscloud.com>
2014-08-12 20:02:32 -0700

    New Task Typpe

---
+++ King.Service/SelfGoverningTask.cs
@@ -0,0 +1,20 @@
+﻿namespace King.Service
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// A task that determines its frequency through actions taken
+    /// </summary>
+    /// <remarks>
+    /// If there is work is to be done then the task gets more aggressive
+    /// If there is no work to be done then the task backs off
+    /// - The result should be that the task is working at a needed capacity.
+    /// </remarks>
+    public class SelfGoverningTask
+    {
+    }
+}
