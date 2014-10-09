bbd4ffc72c7496eec3928169dccce11d88cdd34e
Jef King <jef.king@trader.ca>
2014-10-09 16:42:21 -0700

    Assembly

--- King.Service/Properties/AssemblyInfo.cs
+++ King.Service/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("b2d41b3d-e0c9-4881-b09e-8e9bb4520341")]
-[assembly: AssemblyVersion("1.4.1.9")]
-[assembly: AssemblyFileVersion("1.4.1.9")]
+[assembly: AssemblyVersion("1.4.1.10")]
+[assembly: AssemblyFileVersion("1.4.1.10")]
