b2ca096f4c7e702665324ae065a1510b2d05c584
Jef King <jef@agilebusinesscloud.com>
2014-10-28 22:13:35 -0700

    yield return!

--- Worker/Factory.cs
+++ Worker/Factory.cs
@@ -13,49 +13,47 @@
             //Load configuration to pass to objects
             var connectionString = "UseDevelopmentStorage=true;";
 
-            //Build a manifest of Tasks to run
-            var tasks = new List<IRunnable>();
-
             // Initialization Task(s)
-            tasks.Add(new InitTask());
+            yield return new InitTask();
 
             // Initialize Table; creates table if it doesn't already exist
             var table = new TableStorage("table", connectionString);
-            tasks.Add(new InitializeStorageTask(table));
+            yield return new InitializeStorageTask(table);
 
             // Initialize Queue; creates queue if it doesn't already exist
             var queue = new StorageQueue("queue", connectionString);
-            tasks.Add(new InitializeStorageTask(queue));
+            yield return new InitializeStorageTask(queue);
 
             // Initialize Queue; creates queue if it doesn't already exist
             var container = new Container("container", connectionString);
-            tasks.Add(new InitializeStorageTask(container));
+            yield return new InitializeStorageTask(container);
 
             //Task(s)
-            tasks.Add(new Task());
+            yield return new Task();
 
             //Cordinated Tasks between Instances
             var task = new Coordinated(connectionString);
 
             // Add once to ensure that Table is created for Instances to communicate with
-            tasks.Add(task.InitializeTask());
+            yield return task.InitializeTask();
 
             // Add your coordinated task(s)
-            tasks.Add(task);
+            yield return task;
 
             //Backoff task(s)
-            tasks.Add(new Backoff());
+            yield return new Backoff();
 
             //Self Governing task(s)
-            tasks.Add(new Adaptive());
+            yield return new Adaptive();
 
-            //Dequeue task(s)
-            tasks.Add(new BackoffRunner(new CompanyDequeuer(queue)));
+            //Dequeue task(s), Backoff
+            yield return new BackoffRunner(new CompanyDequeuer(queue));
+
+            //Dequeue task(s), Adaptive
+            yield return new AdaptiveRunner(new CompanyDequeuer(queue));
 
             //Task for Queuing
-            tasks.Add(new CompanyQueuer());
-
-            return tasks;
+            yield return new CompanyQueuer();
         }
     }
 }
