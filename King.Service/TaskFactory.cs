0f36efcbeb2742f19f77735937f7e2152c4d4215
Jef King <jef@agilebusinesscloud.com>
2014-10-30 20:35:05 -0700

    marked for removal

--- King.Service/TaskFactory.cs
+++ King.Service/TaskFactory.cs
@@ -1,10 +1,12 @@
 ﻿namespace King.Service
 {
+    using System;
     using System.Collections.Generic;
 
     /// <summary>
     /// Service Factory
     /// </summary>
+    [Obsolete("Just use interface King.Service.ITaskFactory<T>")]
     public abstract class TaskFactory : ITaskFactory<object>
     {
         #region Methods
