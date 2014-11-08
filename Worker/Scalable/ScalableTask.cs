621b278807db7e52bb1054dfffbd142a1529ee75
Jef King <jef@agilebusinesscloud.com>
2014-11-08 10:17:01 -0800

    Scale up and down in demo

--- Worker/Scalable/ScalableTask.cs
+++ Worker/Scalable/ScalableTask.cs
@@ -1,6 +1,7 @@
 ﻿namespace Worker.Scalable
 {
     using King.Service;
+    using System;
     using System.Diagnostics;
     using System.Threading.Tasks;
 
@@ -8,18 +9,27 @@
     {
         public int MaximumPeriodInSeconds
         {
-            get { return 30; }
+            get
+            {
+                return 30;
+            }
         }
 
         public int MinimumPeriodInSeconds
         {
-            get { return 20; }
+            get
+            {
+                return 20;
+            }
         }
 
         public Task<bool> Run()
         {
-            Trace.TraceInformation("Scalable Task Running.");
-            return Task.FromResult(true);
+            var random = new Random();
+            var workWasDone = (random.Next() % 2) == 0;
+
+            Trace.TraceInformation("Work was done: {0}", workWasDone);
+            return Task.FromResult(workWasDone);
         }
     }
 }
