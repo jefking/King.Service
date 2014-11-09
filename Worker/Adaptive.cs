7730294268e90e2c2ae479f3331a87441d5150b3
Jef King <jef@agilebusinesscloud.com>
2014-11-08 21:47:12 -0800

    Bring the tasks back

--- Worker/Adaptive.cs
+++ Worker/Adaptive.cs
@@ -6,12 +6,8 @@
 
     public class Adaptive : AdaptiveTask
     {
-        public Adaptive()
-            : base(1, 120) { }
         public override void Run(out bool workWasDone)
         {
-            Trace.TraceInformation("A:{0}", System.DateTime.UtcNow.TimeOfDay);
-
             var random = new Random();
             workWasDone = (random.Next() % 2) == 0;
 
--- Worker/Backoff.cs
+++ Worker/Backoff.cs
@@ -6,14 +6,10 @@
 
     public class Backoff : BackoffTask
     {
-        public Backoff()
-            : base(1, 120) { }
         public override void Run(out bool workWasDone)
         {
-            Trace.TraceInformation("B:{0}", System.DateTime.UtcNow.TimeOfDay);
-
             var random = new Random();
-            workWasDone = base.Every.TotalSeconds == 120;
+            workWasDone = (random.Next() % 2) == 0;
 
             Trace.TraceInformation("Work was done: {0}", workWasDone);
         }
