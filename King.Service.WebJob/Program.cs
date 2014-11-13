2c23fd8269dae1c2b08ca371121e2265d496731f
Jef King <jef@agilebusinesscloud.com>
2014-11-12 20:27:01 -0800

    WebJob

---
+++ King.Service.WebJob/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace King.Service.WebJob
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // To learn more about Microsoft Azure WebJobs, please see http://go.microsoft.com/fwlink/?LinkID=401557
+    public class Program
+    {
+        public static void Main()
+        {
+        }
+    }
+}
