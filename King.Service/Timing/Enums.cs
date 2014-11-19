9304b82922b5046df4bf142ea1b562a0927298f4
Jef King <jef@agilebusinesscloud.com>
2014-11-19 05:50:26 -0800

    strategy enum

---
+++ King.Service/Timing/Enums.cs
@@ -0,0 +1,17 @@
+﻿namespace King.Service.Timing
+{
+    /// <summary>
+    /// Timing Strategy
+    /// </summary>
+    public enum Strategy : byte
+    {
+        /// <summary>
+        /// Exponential, Default
+        /// </summary>
+        Exponential = 0,
+        /// <summary>
+        /// Linear
+        /// </summary>
+        Linear = 1,
+    }
+}
