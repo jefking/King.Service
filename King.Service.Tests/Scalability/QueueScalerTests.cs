efaa05315223d8271d358f8452ea6a84a1d243f6
Jef King <jef@agilebusinesscloud.com>
2014-12-05 22:59:22 -0800

    Queue Scaler

---
+++ King.Service.Tests/Scalability/QueueScalerTests.cs
@@ -0,0 +1,89 @@
+﻿namespace King.Service.Tests.Scalability
+{
+    using King.Azure.Data;
+    using King.Service.Scalability;
+    using NSubstitute;
+    using NUnit.Framework;
+    using System;
+    using System.Threading.Tasks;
+
+    [TestFixture]
+    public class QueueScalerTests
+    {
+        [Test]
+        public void Constructor()
+        {
+            var queue = Substitute.For<IQueueCount>();
+            new QueueScaler<object>(queue);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ConstructorQueueNull()
+        {
+            new QueueScaler<object>(null);
+        }
+
+        [Test]
+        public void Maximum()
+        {
+            var random = new Random();
+            var max = (ushort)random.Next(ushort.MinValue, ushort.MaxValue);
+            var queue = Substitute.For<IQueueCount>();
+            var qs = new QueueScaler<object>(queue, max);
+            Assert.AreEqual(max, qs.Maximum);
+        }
+
+        [Test]
+        public void MaximumMinimum()
+        {
+            var random = new Random();
+            var max = (ushort)random.Next(ushort.MinValue, 9);
+            var queue = Substitute.For<IQueueCount>();
+            var qs = new QueueScaler<object>(queue, max);
+            Assert.AreEqual(100, qs.Maximum);
+        }
+
+        [Test]
+        public void ShouldScaleTrue()
+        {
+            var queue = Substitute.For<IQueueCount>();
+            queue.ApproixmateMessageCount().Returns(Task.FromResult<long?>(10000));
+
+            var qs = new QueueScaler<object>(queue);
+            var result = qs.ShouldScale();
+
+            Assert.IsTrue(result.Value);
+
+            queue.Received().ApproixmateMessageCount();
+        }
+
+        [Test]
+        public void ShouldScaleFalse()
+        {
+            var queue = Substitute.For<IQueueCount>();
+            queue.ApproixmateMessageCount().Returns(Task.FromResult<long?>(1));
+
+            var qs = new QueueScaler<object>(queue);
+            var result = qs.ShouldScale();
+
+            Assert.IsFalse(result.Value);
+
+            queue.Received().ApproixmateMessageCount();
+        }
+
+        [Test]
+        public void ShouldScaleThrows()
+        {
+            var queue = Substitute.For<IQueueCount>();
+            queue.When(q => q.ApproixmateMessageCount()).Do(x => { throw new Exception(); });
+
+            var qs = new QueueScaler<object>(queue);
+            var result = qs.ShouldScale();
+
+            Assert.IsFalse(result.HasValue);
+
+            queue.Received().ApproixmateMessageCount();
+        }
+    }
+}
---
+++ King.Service/Scalability/QueueScaler.cs
@@ -0,0 +1,76 @@
+﻿namespace King.Service.Scalability
+{
+    using King.Azure.Data;
+    using System;
+    using System.Diagnostics;
+
+    /// <summary>
+    /// Queue Scaler
+    /// </summary>
+    /// <typeparam name="T">Type</typeparam>
+    public class QueueScaler<T> : Scaler<T>
+    {
+        #region Members
+        /// <summary>
+        /// Maximum
+        /// </summary>
+        protected readonly ushort maximum = 100;
+
+        /// <summary>
+        /// Queue Count
+        /// </summary>
+        protected readonly IQueueCount queueCount = null;
+        #endregion
+        
+        #region Constructors
+        /// <summary>
+        /// Mockable Constructor
+        /// </summary>
+        /// <param name="units">Scale Units</param>
+        public QueueScaler(IQueueCount queueCount, ushort maximum = 100)
+        {
+            if (null == queueCount)
+            {
+                throw new ArgumentNullException("queueCount");
+            }
+
+            this.queueCount = queueCount;
+            this.maximum = maximum < 10 ? (ushort)100 : maximum;
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Maximum
+        /// </summary>
+        public virtual ushort Maximum
+        {
+            get
+            {
+                return this.maximum;
+            }
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Should Scale
+        /// </summary>
+        /// <returns>Direction</returns>
+        public override bool? ShouldScale()
+        {
+            try
+            {
+                var result = queueCount.ApproixmateMessageCount().Result;
+                return result > maximum;
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError(ex.ToString());
+
+                return null;
+            }
+        }
+        #endregion
+    }
+}
