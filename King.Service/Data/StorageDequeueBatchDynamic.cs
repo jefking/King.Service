7d837f030ed682624ff61482331b4ce989964890
Jef King <jef@agilebusinesscloud.com>
2014-12-13 16:51:54 -0800

    Add class, comments on timings

---
+++ King.Service/Data/StorageDequeueBatchDynamic.cs
@@ -0,0 +1,14 @@
+﻿namespace King.Service.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class StorageDequeueBatchDynamic
+    {
+        //maximum batchsize = 32
+        //Set the visibilty in the call to get the batches.
+    }
+}
