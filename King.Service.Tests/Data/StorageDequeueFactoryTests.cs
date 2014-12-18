1690ee46633d8d6c0a4ca0e5245520527b732d31
Jef King <jef@agilebusinesscloud.com>
2014-12-18 07:22:42 -0800

    Testing

---
+++ King.Service.Tests/Data/StorageDequeueFactoryTests.cs
@@ -0,0 +1,14 @@
+﻿namespace King.Service.Tests.Data
+{
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [TestFixture]
+    public class StorageDequeueFactoryTests
+    {
+    }
+}
---
+++ King.Service.Tests/Data/StorageQueueAutoScalerTests.cs
@@ -0,0 +1,56 @@
+﻿namespace King.Service.Tests.Data
+{
+    using King.Azure.Data;
+    using King.Service.Data;
+    using King.Service.Data.Model;
+    using NSubstitute;
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [TestFixture]
+    public class StorageQueueAutoScalerTests
+    {
+        class Setup : QueueSetup<object>
+        {
+            public override IProcessor<object> Get()
+            {
+                return Substitute.For<IProcessor<object>>();
+            }
+        }
+
+        [Test]
+        public void Constructor()
+        {
+            new StorageDequeueFactory<object>();
+        }
+
+        [Test]
+        public void IsITaskFactory()
+        {
+            Assert.IsNotNull(new StorageDequeueFactory<object>() as ITaskFactory<IQueueSetup<object>>);
+        }
+
+        [Test]
+        public void Tasks()
+        {
+            var setup = new Setup();
+            var f = new StorageDequeueFactory<object>();
+            var tasks = f.Tasks(setup);
+
+            Assert.IsNotNull(tasks);
+            Assert.AreEqual(2, tasks);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TasksSetupNull()
+        {
+            var f = new StorageDequeueFactory<object>();
+            f.Tasks(null);
+        }
+    }
+}
