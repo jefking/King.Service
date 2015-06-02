7f6912708ef0eb549faf724bbd187e3b6ae8d1b1
Jef King <jefkin@microsoft.com>
2015-06-01 20:03:03 -0700

    Tests

---
+++ King.Service.Tests/Data/QueueSimplifiedScalerTests.cs
@@ -0,0 +1,14 @@
+﻿namespace King.Service.Tests.Data
+{
+    using King.Azure.Data;
+    using King.Service.Data;
+    using NSubstitute;
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+
+    [TestFixture]
+    public class QueueSimplifiedScalerTests
+    {
+    }
+}
