3c242048735dd29c82d039b75fa43f2acb40f242
Jef King <jefkin@microsoft.com>
2016-05-08 10:35:12 -0700

    Single Task Factory

---
+++ King.Service/SingleTaskFactory.cs
@@ -0,0 +1,47 @@
+﻿namespace King.Service
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Create Single Task, perfect for micro-services
+    /// </summary>
+    /// <typeparam name="T">Passthrough</typeparam>
+    public class SingleTaskFactory<T> : ITaskFactory<T>
+    {
+        #region Members
+        /// <summary>
+        /// Creates Task on Demand
+        /// </summary>
+        protected readonly Func<T, IRunnable> create;
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="create">Create</param>
+        public SingleTaskFactory(Func<T, IRunnable> create)
+        {
+            if (null == create)
+            {
+                throw new ArgumentNullException("create is null.");
+            }
+
+            this.create = create;
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Build Task Manifest
+        /// </summary>
+        /// <param name="passthrough">Configuration</param>
+        /// <returns>Runnable Tasks</returns>
+        public IEnumerable<IRunnable> Tasks(T passthrough)
+        {
+            yield return this.create(passthrough);
+        }
+        #endregion
+    }
+}
