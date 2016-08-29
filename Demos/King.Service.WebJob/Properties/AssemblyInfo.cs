cc48cfa0e24b598eb2b619a796d283e46494be26
Jef King <jefkin@microsoft.com>
2016-08-28 19:21:37 -0700

    misc

--- Demos/King.Service.WebJob/Properties/AssemblyInfo.cs
+++ Demos/King.Service.WebJob/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("545e5f1d-0436-4d90-aab8-3582dd0af173")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
