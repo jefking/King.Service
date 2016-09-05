d62fdb374594fae078924c789bc1e88caf918835
Jef King <jefkin@microsoft.com>
2016-09-05 16:01:49 -0700

    HappensEvery

---
+++ King.Service.Unit.Tests/HappensEveryAttributeTests.cs
@@ -0,0 +1,31 @@
+﻿namespace King.Service.Unit.Tests
+{
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture]
+    public class HappensEveryAttributeTests
+    {
+        [Test]
+        public void Constructor()
+        {
+            var random = new Random();
+            new HappensEveryAttribute(random.Next(1, 1000));
+        }
+
+        [Test]
+        public void ConstructorFrequencyLow()
+        {
+            Assert.That(() => new HappensEveryAttribute(-1), Throws.TypeOf<ArgumentException>());
+        }
+
+        [Test]
+        public void Frequency()
+        {
+            var random = new Random();
+            var f = random.Next(1, 1000);
+            var attr = new HappensEveryAttribute(f);
+            Assert.AreEqual(f, attr.Frequency);
+        }
+    }
+}
---
+++ King.Service/HappensEveryAttribute.cs
@@ -0,0 +1,41 @@
+﻿namespace King.Service
+{
+    using System;
+    using Timing;
+
+    /// <summary>
+    /// Remarks:
+    /// Must be placed on class with a default constructor
+    /// Must be placed on a method with no parameters
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public class HappensEveryAttribute : Attribute
+    {
+        #region Members
+        /// <summary>
+        /// Frequency in Seconds
+        /// </summary>
+        public int Frequency
+        {
+            get;
+            private set;
+        }
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="frequency">Frequency in Seconds</param>
+        public HappensEveryAttribute(int frequency = BaseTimes.DefaultMaximumTiming)
+        {
+            if (0 >= frequency)
+            {
+                throw new ArgumentException("Frequency must be greater than 0.");
+            }
+
+            this.Frequency = frequency;
+        }
+        #endregion
+    }
+}
