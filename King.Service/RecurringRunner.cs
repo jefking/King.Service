3147546504343af7003cfe1ad99507104f078f11
Jef King <jefkin@microsoft.com>
2016-09-04 21:29:39 -0700

    using

--- King.Service/RecurringRunner.cs
+++ King.Service/RecurringRunner.cs
@@ -1,7 +1,7 @@
-﻿using System.Threading.Tasks;
-
-namespace King.Service
+﻿namespace King.Service
 {
+    using System.Threading.Tasks;
+
     /// <summary>
     /// Recurring Runner
     /// </summary>
