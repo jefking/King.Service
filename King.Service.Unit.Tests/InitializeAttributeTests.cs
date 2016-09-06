e410c4e267d24ca3ef80223c119f4ff051de79a9
Jef King <jefkin@microsoft.com>
2016-09-05 17:50:02 -0700

    Init Attribute

---
+++ King.Service.Unit.Tests/InitializeAttributeTests.cs
@@ -0,0 +1,21 @@
+﻿namespace King.Service.Unit.Tests
+{
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture]
+    public class InitializeAttributeTests
+    {
+        [Test]
+        public void Constructor()
+        {
+            new InitializeAttribute();
+        }
+
+        [Test]
+        public void IsAttribute()
+        {
+            Assert.IsNotNull(new InitializeAttribute() as Attribute);
+        }
+    }
+}
---
+++ King.Service/InitializeAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace King.Service
+{
+    using System;
+
+    /// <summary>
+    /// Remarks:
+    /// Must be placed on class with a default constructor
+    /// Must be placed on a method with no parameters
+    /// Must be placed on method that returns bool (work was done)
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public class InitializeAttribute : Attribute
+    {
+    }
+}
