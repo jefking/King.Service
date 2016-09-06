a1b82bc3484d211238e9ec4062d67df9a1731837
Jef King <jefkin@microsoft.com>
2016-09-05 17:52:56 -0700

    404 test cases!

---
+++ King.Service.Unit.Tests/InitializeRunnerTests.cs
@@ -0,0 +1,52 @@
+﻿namespace King.Service.Unit.Tests
+{
+    using NSubstitute;
+    using NUnit.Framework;
+    using System;
+    using System.Threading.Tasks;
+
+    [TestFixture]
+    public class InitializeRunnerTests
+    {
+        [Test]
+        public void Constructor()
+        {
+            var method = typeof(object).GetMethod("ToString");
+            new InitializeRunner(new object(), method);
+        }
+
+        [Test]
+        public void ConstructorInstanceNull()
+        {
+            var method = typeof(object).GetMethod("ToString");
+            Assert.That(() => new InitializeRunner(null, method), Throws.TypeOf<ArgumentNullException>());
+        }
+
+        [Test]
+        public void ConstructorMethodNull()
+        {
+            var method = typeof(object).GetMethod("ToString");
+            Assert.That(() => new InitializeRunner(new object(), null), Throws.TypeOf<ArgumentNullException>());
+        }
+
+        [Test]
+        public void IsInitializeTask()
+        {
+            var method = typeof(object).GetMethod("ToString");
+            Assert.IsNotNull(new InitializeRunner(new object(), method) as InitializeTask);
+        }
+        
+        [Test]
+        public void Run()
+        {
+            var instance = Substitute.For<object>();
+
+            var method = instance.GetType().GetMethod("ToString");
+            var runs = new InitializeRunner(instance, method);
+
+            runs.Run();
+
+            instance.Received(1).ToString();
+        }
+    }
+}
---
+++ King.Service/InitializeRunner.cs
@@ -0,0 +1,55 @@
+﻿namespace King.Service
+{
+    using System;
+    using System.Reflection;
+
+    /// <summary>
+    /// Initialize Runner
+    /// </summary>
+    public class InitializeRunner : InitializeTask
+    {
+        #region Members
+        /// <summary>
+        /// Instance
+        /// </summary>
+        protected readonly object instance;
+
+        /// <summary>
+        /// Method
+        /// </summary>
+        protected readonly MethodInfo method;
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="instance">Instance</param>
+        /// <param name="method">Method</param>
+        public InitializeRunner(object instance, MethodInfo method)
+        {
+            if (null == instance)
+            {
+                throw new ArgumentNullException("Instance can not be null.");
+            }
+            if (null == method)
+            {
+                throw new ArgumentNullException("Method for invocation can not be null.");
+            }
+
+            this.instance = instance;
+            this.method = method;
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Run
+        /// </summary>
+        public override void Run()
+        {
+            var result = this.method.Invoke(instance, null);
+        }
+        #endregion
+    }
+}
