e0777269d1eceff44dd5a6f352530db07d4cc065
Jef King <jefkin@microsoft.com>
2016-09-06 20:01:36 -0700

    tighten code up

--- King.Service/TaskFinderFactory.cs
+++ King.Service/TaskFinderFactory.cs
@@ -6,6 +6,7 @@
     using System.Reflection;
     using System.Threading.Tasks;
     using Timing;
+    using System.Linq;
 
     /// <summary>
     /// Task Finder, searches assembly for attribute based tasks.
@@ -24,47 +25,48 @@
             var assembly = typeof(T).GetTypeInfo().Assembly;
 
             var runnables = new List<IRunnable>();
-            Parallel.ForEach(assembly.DefinedTypes, type =>
+            var types = from t in assembly.DefinedTypes
+                        where t.IsClass && t.IsPublic
+                        select t;
+
+            Parallel.ForEach(types, type =>
             {
-                if (type.IsClass && type.IsPublic)
+                foreach (var method in type.DeclaredMethods)
                 {
-                    foreach (var method in type.DeclaredMethods)
+                    foreach (var everyAttr in method.GetCustomAttributes(typeof(InitializeAttribute), false))
                     {
-                        foreach (var everyAttr in method.GetCustomAttributes(typeof(InitializeAttribute), false))
+                        Trace.TraceInformation("Initialization task found: {0}.{1}", type.FullName, method.Name);
+
+                        var instance = Activator.CreateInstance(type.AsType());
+                        runnables.Add(new InitializeRunner(instance, method));
+                    }
+
+                    foreach (var everyAttr in method.GetCustomAttributes(typeof(RunsEveryAttribute), false))
+                    {
+                        Trace.TraceInformation("Runs Every task found: {0}.{1}", type.FullName, method.Name);
+
+                        var every = everyAttr as RunsEveryAttribute;
+                        var instance = Activator.CreateInstance(type.AsType());
+                        var run = new EveryRuns(instance, method, every.Frequency);
+                        runnables.Add(new RecurringRunner(run));
+                    }
+
+                    foreach (var betweenAttr in method.GetCustomAttributes(typeof(RunsBetweenAttribute), false))
+                    {
+                        Trace.TraceInformation("Runs between task found: {0}.{1}", type.FullName, method.Name);
+
+                        var between = betweenAttr as RunsBetweenAttribute;
+                        var instance = Activator.CreateInstance(type.AsType());
+                        var run = new BetweenRuns(instance, method, between.Frequency.Minimum, between.Frequency.Maximum);
+                        switch (between.Strategy)
                         {
-                            Trace.TraceInformation("Initialization task found: {0}.{1}", type.FullName, method.Name);
-
-                            var instance = Activator.CreateInstance(type.AsType());
-                            runnables.Add(new InitializeRunner(instance, method));
-                        }
-
-                        foreach (var everyAttr in method.GetCustomAttributes(typeof(RunsEveryAttribute), false))
-                        {
-                            Trace.TraceInformation("Runs Every task found: {0}.{1}", type.FullName, method.Name);
-
-                            var every = everyAttr as RunsEveryAttribute;
-                            var instance = Activator.CreateInstance(type.AsType());
-                            var run = new EveryRuns(instance, method, every.Frequency);
-                            runnables.Add(new RecurringRunner(run));
-                        }
-
-                        foreach (var betweenAttr in method.GetCustomAttributes(typeof(RunsBetweenAttribute), false))
-                        {
-                            Trace.TraceInformation("Runs between task found: {0}.{1}", type.FullName, method.Name);
-
-                            var between = betweenAttr as RunsBetweenAttribute;
-                            var instance = Activator.CreateInstance(type.AsType());
-                            var run = new BetweenRuns(instance, method, between.Frequency.Minimum, between.Frequency.Maximum);
-                            switch (between.Strategy)
-                            {
-                                case Strategy.Exponential:
-                                    runnables.Add(new BackoffRunner(run, between.Strategy));
-                                    break;
-                                case Strategy.Linear:
-                                default:
-                                    runnables.Add(new AdaptiveRunner(run, between.Strategy));
-                                    break;
-                            }
+                            case Strategy.Exponential:
+                                runnables.Add(new BackoffRunner(run, between.Strategy));
+                                break;
+                            case Strategy.Linear:
+                            default:
+                                runnables.Add(new AdaptiveRunner(run, between.Strategy));
+                                break;
                         }
                     }
                 }
