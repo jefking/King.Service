ead5547c4b42b74aafe3ce053953cb75eabe2b0f
Jef King <jefkin@microsoft.com>
2018-06-15 15:14:29 -0700

    versions

--- King.Service.Demo/Program.cs
+++ King.Service.Demo/Program.cs
@@ -4,7 +4,7 @@
 
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
