346af9b9f019263b66ed74457fbe6f5d3852b6cb
Jef King <jefkin@microsoft.com>
2018-06-15 15:10:48 -0700

    Demo

---
+++ King.Service.Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace King.Service.Demo
+{
+    using System;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
