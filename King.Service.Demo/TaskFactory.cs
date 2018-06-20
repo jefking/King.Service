ec256e97b5c294533d3ce7ada636fe456f5940d0
Jef King <jefkin@microsoft.com>
2018-06-20 14:53:58 -0700

    tasks for Demo

--- King.Service.Demo/TaskFactory.cs
+++ King.Service.Demo/TaskFactory.cs
@@ -1,12 +1,22 @@
 ﻿namespace King.Service.Demo
 {
+    using King.Service.Demo.Tasks;
     using System.Collections.Generic;
 
     public class TaskFactory : ITaskFactory<AppConfig>
     {
-        public IEnumerable<IRunnable> Tasks(AppConfig passthrough)
+        public IEnumerable<IRunnable> Tasks(AppConfig config)
         {
-            yield return null;
+            yield return new Adaptive();
+            yield return new Backoff();
+            yield return new Recurring();
+
+            yield return new InitTask();
+
+            yield return new Coordinated(config.ConnectionString);
+            yield return new OnceDaily(config.ConnectionString);
+
+            yield return new CompanyQueuer(config.GenericQueueName, config.ConnectionString);
         }
     }
 }
