eb521dfd16a8bfa146caa36fff2443f6f7b45edb
jef king <jking@atriedes.io>
2022-10-28 10:41:08 -0700

    done

---
+++ King.Service.Azure/Data/InitializeStorageTask.cs
@@ -0,0 +1,51 @@
+﻿namespace King.Service.Data
+{
+    using global::Azure.Data.Wrappers;
+    using King.Service;
+    using System;
+    using System.Diagnostics;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Initialize Storage Task
+    /// </summary>
+    public class InitializeStorageTask : InitializeTask
+    {
+        #region Members
+        /// <summary>
+        /// Azure Storage
+        /// </summary>
+        protected readonly IAzureStorage storage = null;
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Intialize class with Storage to create
+        /// </summary>
+        /// <param name="storage">Storage</param>
+        public InitializeStorageTask(IAzureStorage storage)
+        {
+            if (null == storage)
+            {
+                throw new ArgumentNullException("storage");
+            }
+
+            this.storage = storage;
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Run, Creates Table
+        /// </summary>
+        public override async Task RunAsync()
+        {
+            Trace.TraceInformation("Creating: '{0}'.", this.storage.Name);
+
+            var result = await this.storage.CreateIfNotExists();
+
+            Trace.TraceInformation("Created: '{0}'; was created {1}.", this.storage.Name, result);
+        }
+        #endregion
+    }
+}
